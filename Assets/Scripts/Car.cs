4f9588be7356c87e674aaff6eba5aadae02963ca
eitanoodle <ezra.eitan@gmail.com>
2022-04-18 20:06:46 +0300

    Implemented boosts and slow-downs when you hit a wall.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -2,26 +2,37 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] private float SteerSpeed = 0.5f;
-    [SerializeField] private float MoveSpeed = 0.5f;
+    [SerializeField] private float steerSpeed = 0.5f;
+    [SerializeField] private float moveSpeed = 0.25f;
+    [SerializeField] private float slowSpeed = 0.15f;
+    [SerializeField] private float boostSpeed = 0.30f;
+
+    private float currSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currSpeed = moveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void FixedUpdate()
+   void FixedUpdate()
     {
         float SteerAmount = Input.GetAxis("Horizontal");
         float DriveAmount = Input.GetAxis("Vertical");
-        transform.Rotate(0, 0, -SteerAmount * SteerSpeed);
-        transform.Translate(0, DriveAmount * MoveSpeed, 0);
+        transform.Rotate(0, 0, -SteerAmount * steerSpeed);
+        transform.Translate(0, DriveAmount * currSpeed, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Booster")
+        {
+            currSpeed = boostSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currSpeed = moveSpeed;
     }
 }
